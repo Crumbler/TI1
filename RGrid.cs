ece3b39c75cffbc91032e5b5c05f2c8262a5822f
Crumbler <25861574+Crumbler@users.noreply.github.com>
2021-03-05 11:12:11 +0300

    RGrid Decode

--- RGrid.cs
+++ RGrid.cs
@@ -8,28 +8,47 @@ namespace TI1
     {
         public static void Switch()
         {
+            Console.WriteLine("Enter the message:");
+            string msg = Console.ReadLine();
+
+            // Side length of the square matrix
+            int depth = (int)Math.Ceiling(Math.Sqrt(msg.Length));
+
+            // Number of elements in the square matrix
+            int sqrDepth = depth * depth;
+
+            // Number of elements in one of 4 rectangles
+            int rCount = sqrDepth / 4;
+
+            Console.WriteLine("Depth: " + depth);
+
+            Console.WriteLine("Enter {0} numbers from 1 to 4", rCount);
+            string sNums = Console.ReadLine();
+            if (sNums.Any(c => c < '1' || c > '4') || sNums.Length != rCount)
+            {
+                Console.WriteLine("Invalid sequence");
+                return;
+            }
+
             Console.WriteLine("Enter 1 to encode");
             Console.WriteLine("Enter 2 to decode");
-
             int command = Convert.ToInt32(Console.ReadLine());
 
             switch (command)
             {
             case 1:
-                Console.WriteLine("Enter the message to encode:");
-                string s1 = Console.ReadLine();
-
-                int dim = s1.Length / 4;
-                Console.WriteLine("Enter {0} quadrant number", s1.Length - s1.Length % 2);
-                string key = Console.ReadLine();
-
-                Console.WriteLine(Encode(s1, key));
+                try
+                {
+                    Console.WriteLine(Encode(msg, sNums, rCount, depth));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
                 break;
 
             case 2:
-                Console.WriteLine("Enter the message to decode:");
-                string s2 = Console.ReadLine();
-                Console.WriteLine(Decode(s2, string.Empty));
+                Console.WriteLine(Decode(msg, sNums));
                 break;
 
             default:
@@ -38,9 +57,93 @@ namespace TI1
             }
         }
 
-        private static string Encode(string message, string key)
+        // Rotates a vector clockwise
+        // 1 = 90 degrees
+        private static (int, int) RotCW((int, int) v, int angle)
         {
-            return string.Empty;
+            switch(angle)
+            {
+                case 0:
+                    return v;
+
+                case 1:
+                    return (-v.Item2, v.Item1);
+
+                case 2:
+                    return (-v.Item1, -v.Item2);
+
+                case 3:
+                    return (v.Item2, -v.Item1);
+            }
+
+            return default;
+        }
+
+        private static (int, int) Add((int, int) a, (int, int) b)
+        {
+            return (a.Item1 + b.Item1, a.Item2 + b.Item2);
+        }
+
+        private static string Encode(string message, string key, int rCount, int depth)
+        {
+            int[] rSequence = key.Select(c => (int)(c - '1')).ToArray();
+
+            var corners = new (int, int)[4]
+            {
+                (0, 0),
+                (depth - 1, 0),
+                (depth - 1, depth - 1),
+                (0, depth - 1)
+            };
+
+            // Offsets relative to the rectangle
+            var rOffsets = new (int, int)[rCount];
+
+            int hDepth = depth / 2 + depth % 2;
+            int vDepth = depth / 2;
+
+            for (int i = 0; i < vDepth; ++i)
+                for (int j = 0; j < hDepth; ++j)
+                    rOffsets[i * hDepth + j] = (j, i);
+
+            // Offsets relative to the square matrix
+            var gOffsets = new (int, int)[rCount];
+            for (int i = 0; i < rCount; ++i)
+                gOffsets[i] = Add(corners[rSequence[i]], RotCW(rOffsets[i], rSequence[i]));
+
+            var M = new char[depth, depth];
+
+            for (int i = 0; i < depth; ++i)
+                for (int j = 0; j < depth; ++j)
+                    M[i, j] = ' ';
+
+            int maxChar = message.Length;
+
+            // If the side length is odd, the center element is set separately
+            if (maxChar == depth * depth && maxChar % 2 != 0)
+                --maxChar;
+
+            for (int i = 0; i < maxChar; ++i)
+            {
+                // Choose corner based on i
+                int angle = (i / rCount) % 4;
+                (int, int) basePos = corners[angle];
+                (int, int) offset = RotCW(gOffsets[i % rCount], angle);
+                (int, int) pos = Add(basePos, offset);
+                M[pos.Item2, pos.Item1] = message[i];
+            }
+
+            // If the side length is odd, set the center element separately
+            if (depth % 2 == 1)
+                M[depth / 2, depth / 2] = message[message.Length - 1];
+
+            var res = new StringBuilder();
+
+            for (int i = 0; i < depth; ++i)
+                for (int j = 0; j < depth; ++j)
+                    res.Append(M[i, j]);
+
+            return res.ToString();
         }
 
         private static string Decode(string message, string key)
