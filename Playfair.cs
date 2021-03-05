f059d383f35430a53e15f60634caffe34bfd4f84
Crumbler <25861574+Crumbler@users.noreply.github.com>
2021-03-05 12:16:47 +0300

    Playfair encode base

--- Playfair.cs
+++ Playfair.cs
@@ -14,6 +14,12 @@ namespace TI1
             Console.WriteLine("Enter the key");
             string key = Console.ReadLine();
 
+            if (msg.Any(c => c < 'a' || c > 'z') || key.Any(c => c < 'a' || c > 'z'))
+            {
+                Console.WriteLine("The message and the key must contain only lowercase English letters");
+                return;
+            }
+
             Console.WriteLine("Enter 1 to encode");
             Console.WriteLine("Enter 2 to decode");
             int command = Convert.ToInt32(Console.ReadLine());
@@ -36,6 +42,28 @@ namespace TI1
 
         private static string Encode(string message, string key)
         {
+            var M = new char[5, 5];
+            key = String.Concat(key.Distinct());
+
+            var alphabet = Enumerable.Range(0, 25)
+                                     .Select(i => (char)(i + 'a'));
+
+            string otherLetters = String.Concat(alphabet.Except(key));
+
+            int i = 0;
+            for (; i < key.Length; ++i)
+                M[i / 5, i % 5] = key[i];
+
+            for (int j = 0; i < 25; ++i, ++j)
+                M[i / 5, i % 5] = otherLetters[j];
+
+            for (i = 0; i < 5; ++i)
+            {
+                for (int j = 0; j < 5; ++j)
+                    Console.Write(M[i, j].ToString() + ' ');
+                Console.WriteLine();
+            }
+
             return string.Empty;
         }
 
